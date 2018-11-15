04be794927defc6ab26617d88e116b02461762ae
Matthew Morgan <mmorgan55@cnm.edu>
2018-11-15 15:40:12 -0700

    Overrode onBackPressed for use during an instance of GameFragment.

--- app/src/main/java/edu/cnm/deepdive/triviagame/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/triviagame/controller/MainActivity.java
@@ -2,11 +2,14 @@ package edu.cnm.deepdive.triviagame.controller;
 
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AlertDialog.Builder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import butterknife.ButterKnife;
 import edu.cnm.deepdive.triviagame.R;
 import edu.cnm.deepdive.triviagame.model.db.TriviaDatabase;
+import edu.cnm.deepdive.triviagame.view.GameFragment;
 import edu.cnm.deepdive.triviagame.view.MainFragment;
 import edu.cnm.deepdive.triviagame.view.PostGameFragment;
 
@@ -37,6 +40,22 @@ public class MainActivity extends AppCompatActivity {
           .popBackStack("main", android.support.v4.app.FragmentManager.POP_BACK_STACK_INCLUSIVE);
       getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment)
           .commit();
+    } else if (getSupportFragmentManager()
+        .findFragmentById(R.id.fragment_container) instanceof GameFragment) {
+      AlertDialog.Builder builder = new Builder(this);
+      builder.setTitle("Back to Game Select");
+      builder.setMessage("Are you sure to want to go back to game select?");
+      builder.setPositiveButton("Yes", (dialog, which) -> {
+        Fragment fragment = new MainFragment();
+        getSupportFragmentManager()
+            .popBackStack("main", android.support.v4.app.FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment)
+            .commit();
+      });
+      builder.setNegativeButton("No", (dialog, which) -> {
+        //Nothing here; pressing "No" will default to screen this was called on, which is wanted.
+      });
+      builder.show();
     } else {
       super.onBackPressed();
     }
