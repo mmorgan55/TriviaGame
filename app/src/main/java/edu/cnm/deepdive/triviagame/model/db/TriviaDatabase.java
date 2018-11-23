672206b793ce45a743bc037e8bcf1db358b1e1f5
Matthew Morgan <mmorgan55@cnm.edu>
2018-11-23 14:39:25 -0700

    Changed design of fragment_main

--- app/src/main/java/edu/cnm/deepdive/triviagame/model/db/TriviaDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/triviagame/model/db/TriviaDatabase.java
@@ -28,7 +28,7 @@ import org.apache.commons.text.StringEscapeUtils;
  */
 @Database(
     entities = {TriviaCategory.class, TriviaQuestion.class, TriviaAnswers.class},
-    version = 1,
+    version = 2,
     exportSchema = true
 )
 public abstract class TriviaDatabase extends RoomDatabase {
